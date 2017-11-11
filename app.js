d4b512c1ca035bc9528162a1d5faba7a0c05d413
Alan Shum <alan200994@gmail.com>
2017-11-11 14:47:30 +0800

    done

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const beautify = require("json-beautify");
 
 const COURSE_INPUT = readCouresList();
 const courseDetailsPromiseArray = COURSE_INPUT.courseList.map(getCourseDetails);
 
 Promise.all(courseDetailsPromiseArray)
   .then(allCourseDetails => {
-    console.log(allCourseDetails);
+    console.info(`[APP] Done. ${allCourseDetails.length} records are crawled and saved`);
+    saveJSONtoFile(allCourseDetails, './data/courseDetails.json');
   })
 
 function readCouresList() {
@@ -17,11 +19,12 @@ function readCouresList() {
 
 function getCourseDetails(courseCode) {
   return new Promise((resolve, reject) => {
+    http://prog-crs.ust.hk/ugcourse/2017-18/search?keyword=COMP2711
     request(`http://prog-crs.ust.hk/ugcourse/2017-18/search?keyword=${courseCode}`, function (error, response, html) {
       if (error) return reject(error);
 
       const $ = cheerio.load(html);
-      const courseDetail = { title: null, credit: null, description: null };
+      const courseDetail = { code: courseCode, title: null, credit: null, description: null };
       $('div.container.container-content div.crse-title').each(function(i, element){
         courseDetail.title = $(this).text();
       });
@@ -32,7 +35,18 @@ function getCourseDetails(courseCode) {
         courseDetail.description = $(this).text();
       });
 
+      console.info(`[CRAWLER] crawled ${courseCode}`);
       resolve(courseDetail);
+    });
+  })
+}
+
+function saveJSONtoFile(json, filePath) {
+  return new Promise((resolve, reject) => {
+    const stringData = beautify(json, null, 2, 120);
+    fs.writeFile(filePath, stringData, function (err,data) {
+      if (err) return reject(err);
+      resolve(data);
     });
   })
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# HKUST course details crawler
+
+> Crawl HKUST course info by supplying a list of course Codes
+
+# To Use
+0. Run `yarn install`
+1. Edit `./data/courseList.json'
+2. run `yarn start`
+3. Get your `./data/courseDetails.json`
